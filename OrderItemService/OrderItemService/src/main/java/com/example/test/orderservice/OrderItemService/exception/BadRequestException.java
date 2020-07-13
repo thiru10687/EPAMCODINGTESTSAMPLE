b171fcf5e86d12583f668027f13c4e74070c1b6f
Thirupathi Balijapally <tb031473@broadcom.net>
2020-07-13 09:50:29 +0530

    Final changes made

--- OrderItemService/OrderItemService/src/main/java/com/example/test/orderservice/OrderItemService/exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.orderservice.OrderItemService.exception;
-
-public class BadRequestException extends Exception  {
-	
-	public BadRequestException(String message) {
-		super(message);
-	}
-
-}
